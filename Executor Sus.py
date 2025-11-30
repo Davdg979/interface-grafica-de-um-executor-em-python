67e36c5884dc0596e4466df5ffbfbc087c786199
Davdg979 <davigabrielsouzadossantos1@gmail.com>
2025-11-30 09:55:39 -0400

    Add light and dark theme styles to the app

--- Executor Sus.py
+++ Executor Sus.py
@@ -1,19 +1,31 @@
 from tkinter import *
 from tkinter import ttk
 
+def Style_white():
+    app.configure(background="White")
+    title.configure(foreground="Black", background="White")
+    label1.configure(foreground="Black", background="White")
+    textbox.configure(foreground="Black", background="White")
+
+def Style_black():
+    app.configure(background="Black")
+    title.configure(foreground="White", background="Black")
+    label1.configure(foreground="White", background="Black")
+    textbox.configure(foreground="White", background="Black")
+
 def reset_status():
     label1.config(text='')
 
 def Run():
     label1.config(text='Executando...')
-    app.after(5000, reset_status) 
+    app.after(1000, reset_status) 
 
 def Clear():
     textbox.delete('1.0', 'end')
 
 def Inject():
     label1.config(text='Injetado...')
-    app.after(5000, reset_status)
+    app.after(1000, reset_status)
 
 app = Tk()
 app.title('Executor Sus')
@@ -38,5 +50,11 @@ button2.place(x=500, y=350)
 
 button3 = ttk.Button(app, text='Inject', command=Inject)
 button3.place(x=600, y=350)
+
+style_White = ttk.Button(app, text="Style White", command=Style_white)
+style_White.place(x=100,y=350)
+
+style_Black = ttk.Button(app, text="Style Black", command=Style_black)
+style_Black.place(x=15,y=350)
 
 app.mainloop()
