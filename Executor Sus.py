8d58873de5608b6e5df545f8e9358b5d05eee470
Davdg979 <davigabrielsouzadossantos1@gmail.com>
2025-12-27 00:10:14 -0400

    Change default textbox message to 'Hello, World!'
    Updated text in the textbox to display 'Hello, World!' instead of 'Made by Davi Gabriel'.

--- Executor Sus.py
+++ Executor Sus.py
@@ -21,7 +21,6 @@ def Style_white():
     title.configure(foreground="Black", background="White")
     label1.configure(foreground="Black", background="White")
     textbox.configure(foreground="Black", background="White")
-    
 
 def Style_black():
     app.configure(background="Black")
@@ -54,23 +53,26 @@ def Janela():
     bt5.place(x=100, y=50)
 
 def Dex():
+    textbox.delete('1.0', 'end')
     textbox.insert('1.0', 'loadstring(game:HttpGet("https://obj.wearedevs.net/2/scripts/Dex%20Explorer.lua"))()')
 
 def Yield():
+    textbox.delete('1.0', 'end')
     textbox.insert('1.0', 'loadstring(game:HttpGet("https://obj.wearedevs.net/2/scripts/Infinite%20Yield.lua"))()')
 
 def Fly_V3():
+    textbox.delete('1.0', 'end')
     textbox.insert('1.0', 'loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()')
 
 def Btools():
+    textbox.delete('1.0', 'end')
     textbox.insert('1.0', 'loadstring(game:HttpGet("https://obj.wearedevs.net/2/scripts/BTools.lua"))()')
 
 def Invesible():
+    textbox.delete('1.0', 'end')
     textbox.insert('1.0', 'loadstring(game:HttpGet("https://obj.wearedevs.net/2/scripts/Invisible%20Character.lua"))()')
 
-
 #------------------------------Executor-----------------------------------#
-
 
 current_language = "en"
 
@@ -84,7 +86,6 @@ def pt_Br():
     style_White.configure(text="Estilo Branco")
     pt_BR.configure(text="Português")
     en.configure(text="Inglês")
-    textbox.insert('1.0',  'print("Feito por Davi Gabriel")')
     label1.config(text='') 
 
 def En():
@@ -97,7 +98,6 @@ def En():
     style_White.configure(text="Style White")
     pt_BR.configure(text="Portuguese")
     en.configure(text="English")
-    textbox.insert('1.0', 'print("Made by Davi Gabriel")')
     label1.config(text='')
 
 def reset_status():
@@ -121,6 +121,8 @@ def Inject():
         label1.config(text='Injected...')
     app.after(5000, reset_status)
 
+#------------------------------Configurações do Executor-----------------------------------#
+
 app = Tk()
 app.title('Executor Sus')
 app.geometry('700x400')
@@ -135,7 +137,7 @@ label1.place(x=530, y=5)
 
 textbox = Text(bg='Black', fg='White', font=('Arial'))
 textbox.place(x=15, y=55, width=675, height=250)
-textbox.insert('1.0', 'print("Made by Davi Gabriel")')
+textbox.insert('1.0', 'print("Hello, World!")')
 
 button1 = ttk.Button(app, text='Run', command=Run)
 button1.place(x=400, y=350)
@@ -146,11 +148,12 @@ button2.place(x=500, y=350)
 button3 = ttk.Button(app, text='Inject', command=Inject)
 button3.place(x=600, y=350)
 
+#------------------------------...-----------------------------------#
 style_White = ttk.Button(app, text="Style White", command=Style_white)
 style_White.place(x=0,y=999)
-
 style_Black = ttk.Button(app, text="Style Black", command=Style_black)
 style_Black.place(x=0,y=999)
+#------------------------------...-----------------------------------#
 
 pt_BR = ttk.Button(app, text="Portuguese", command=pt_Br)
 pt_BR.place(x=15,y=320)
